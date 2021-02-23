ee125067e7103f8bac526ece4a3a0cb5f491c0a6
Justin Cho <jchoustin91@gmail.com>
2021-02-23 13:11:41 -0800

    mongoose connect

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config'); //use config variables
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, { //returns a promise, so await
+            useNewUrlParser: true, //deprecation errors
+            useUnifiedTopology: true
+        });
+
+        console.log('MongoDB Connected...')
+    } catch(err) {
+        console.error(err.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB; //to be used in server.js
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express(); //initialize app variable
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running')); //Test server
 
