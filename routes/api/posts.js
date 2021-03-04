e2608b4d1b45155a6f4b5538016dca94b03e3aea
Justin Cho <jchoustin91@gmail.com>
2021-03-03 23:59:15 -0800

    Posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,81 @@
 const express = require('express'); //get express
 const router = express.Router(); //to set up routes
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
+
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private (no token needed)
+router.post('/', [auth, [
+    check('text', 'Text is required')
+        .not()
+        .isEmpty()
+]],
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        }
+
+        const post = new Post(newPost)
+        await post.save();
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 // @route  GET api/posts
-// @desc   Test route
-// @access Public (no token needed)
-router.get('/', (req, res) => res.send('Posts route'));
+// @desc   Get all Posts
+// @access Private (no token needed)
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+// @route  GET api/posts/:id
+// @desc   Get Post by id
+// @access Private (no token needed)
+router.get('/:id', auth, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+})
+
+// @route DELETE api/posts/:id
+// @desc  Delete post by id
+// @access Private
+
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,8 @@ const express = require('express'); //get express
 const router = express.Router(); //to set up routes
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check')
+const request = require('request');
+const config = require('config')
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -204,7 +206,7 @@ router.put('/experience', [auth, [
 // @route  DELETE api/profile/education/:edu_id
 // @desc   Delete education from profile
 // @access Private
-router.delete('/education/:exp_id', auth, async (req, res) => {
+router.delete('/education/:edu_id', auth, async (req, res) => {
     try {
         const profile = await Profile.findOne({ user: req.user.id });
 
@@ -281,6 +283,32 @@ router.put('/education', [auth, [
     }
 
 });
+
+// @route  GET api/profile/github/:username
+// @desc   Get user repos from github
+// @access Public 
+router.get('/github/:username', async (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+
+        request(options, (error, response, body) => {
+            if(error) console.error(error);
+
+            if(response.statusCode !== 200) {
+                return res.status(404).json({ msg: 'No Github profile found'})
+            }
+
+            res.json(JSON.parse(body));
+        })
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
 
 
 module.exports = router;
