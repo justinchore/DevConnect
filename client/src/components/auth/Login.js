4e0239719523647ca96d7aaace78741daf567bdc
Justin Cho <jchoustin91@gmail.com>
2021-03-05 22:22:40 -0800

    Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -13,11 +16,14 @@ const Login = () => {
 
     const onSubmit = e => { //when axios is used, used async
         e.preventDefault();
-      
-        console.log('SUCCESS')
-
+        console.log("here");
+        login(email, password);
     };
 
+    //Redirect if logged in
+    if(isAuthenticated) {
+      return <Redirect to='/dashboard'/>;
+    }
     return (
         <Fragment>
           <h1 className="large text-primary">Log in</h1>
@@ -52,4 +58,13 @@ const Login = () => {
     )
 };
 
-export default Login
+Login.propTypes = {
+  login: propTypes.func.isRequired, //ptfr
+  isAuthenticated: propTypes.bool.isRequired //ptb
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { login })(Login);
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import {connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
-    return (
-    <nav className="navbar bg-dark">    
-      <h1>
-        <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
-      </h1>
+const Navbar = ({ auth: { isAuthenticated, loading}, logout }) => {
+  const authLinks = (
+      <ul>
+        <a onClick={logout} href="#!">
+            <i className="fas fa-sign-out-alt"></i>{' '}
+            <span className="hide-sm">Logout</span>
+        </a>
+      </ul>
+  );
+
+  const guestLinks = (
       <ul>
         <li><a href="!#">Developers</a></li>
         <li><Link to="/register">Register</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
-    </nav>
+  );
+
+    return (
+    <nav className="navbar bg-dark">    
+      <h1>
+        <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
+      </h1>
+      { !loading && (<Fragment>{isAuthenticated? authLinks : guestLinks }</Fragment>)}
+    </nav> // we could use a && if there is no else
     )
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(Navbar);
 
