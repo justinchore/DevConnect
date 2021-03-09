1eaf8007263eff9fd435064e31144654886aa4f3
Justin Cho <jchoustin91@gmail.com>
2021-03-08 18:17:35 -0800

    Create Profile

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Alert from './components/layouts/Alert';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard.js';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 //Redux
 import { Provider } from 'react-redux'; //connects react and redux
@@ -38,6 +39,7 @@ const App = () => {
               <Route exact path="/register" component={Register}/>
               <Route exact path="/login" component={Login}/>
               <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+              <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
             </Switch>
           </section>
         </Fragment>
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,16 +1,17 @@
-import { SET_ALERT, REMOVE_ALERT} from '../actions/types';
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 const initialState = [];
-//Each object in this array will have an 
+//Each object in this array will have an
 //id, msg, alertType
 
-export default function(state = initialState, action) { //actions will have type, payload
-    const { type, payload } = action;
-    switch(type) {
-        case SET_ALERT: //we need to return changes to the state
-            return [...state, payload]; //state is immutable, so include any state present
-        case REMOVE_ALERT:
-            return state.filter(alert => alert.id !== payload); //we control payload in dispatch
-        default:
-            return state;
-    }
+export default function foo(state = initialState, action) {
+  //actions will have type, payload
+  const { type, payload } = action;
+  switch (type) {
+    case SET_ALERT: //we need to return changes to the state
+      return [...state, payload]; //state is immutable, so include any state present
+    case REMOVE_ALERT:
+      return state.filter(alert => alert.id !== payload); //we control payload in dispatch
+    default:
+      return state;
+  }
 }
